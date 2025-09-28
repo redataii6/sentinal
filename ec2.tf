585af668987a9c7c62190ea938d0c54121eb8d6a
redataii6 <45831615+redataii6@users.noreply.github.com>
2025-09-28 16:31:54 +0100

    ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0ca4d5db4872d0c28"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "sentinal_check"
+  }  
+}
