7ee2f55a9a2a07feeb73230a315e157867ae3e08
redataii6 <45831615+redataii6@users.noreply.github.com>
2025-09-28 16:56:43 +0100

    Change EC2 instance type to t2.small and add tags

--- ec2.tf
+++ ec2.tf
@@ -4,5 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-0ca4d5db4872d0c28"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
+  tags = {
+    Name = "sentinal_check"
+  }
 }
